f6ce283633ef18b7c717543d3e409242d02c5c2b
Martín Vázquez Torres <marvazto@gmail.com>
2017-08-18 12:44:23 +0200

    Restored work from backup. Missing views

---
+++ JJOOxamarin/JJOOxamarin/JJOOxamarin.Model/Sede.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJOOxamarin.model
+{
+    public class Sede
+    {
+        public int id_ciudad { get; private set; }
+        public int anyo { get; private set; }
+        public int tipo_jjoo { get; private set; }
+        public String descripcion_tipo { get; private set; }
+
+        public Sede(int id_ciudad, int anyo, int tipo_jjoo, String descripcion_tipo = "")
+        {
+            this.id_ciudad = id_ciudad;
+            this.anyo = anyo;
+            this.tipo_jjoo = tipo_jjoo;
+            this.descripcion_tipo = descripcion_tipo;
+        }
+
+
+    }
+}
---
+++ JJOOxamarin/JJOOxamarin/JJOOxamarin.Services/RestConsumer.cs
@@ -0,0 +1,43 @@
+﻿using JJOOxamarin.model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JJOOxamarin.REST
+{
+    public static class RestConsumer
+    {
+        private static String baseRestURL = "http://172.26.80.76:8080/";
+
+        public static async Task<List<Olimpiada>> GetCiudadesCompleto()
+        {
+            List<Olimpiada> toret = null;
+            toret = await GetResource<Olimpiada>("olimpiadas");
+            return toret;
+        }
+
+        public static async Task<List<Sede>> GetSedesAsync()
+        {
+            List<Sede> toret = null;
+            toret = await GetResource<Sede>("sedes");
+            return null;
+        }
+
+        private static async Task<List<T>> GetResource<T>(string v)
+        {
+            HttpClient client = new HttpClient();
+            var uri = new Uri(baseRestURL + v);
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var toret = JsonConvert.DeserializeObject<List<T>>(content);
+                return toret;
+            }
+            return new List<T>();
+        }
+
+    }
+}
